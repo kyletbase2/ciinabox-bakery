50ca1005c188393f23fe7b253f0fb1b4a0f9629c
Kyle Thorne <k.thorne@base2services.com>
2021-11-04 16:13:26 +1100

    Added sleep time

--- scripts/win2016/download_cookbooks.ps1
+++ scripts/win2016/download_cookbooks.ps1
@@ -21,7 +21,7 @@ try
 
   try {
     Write-Output "INFO: Sleepy time"
-    Start-Sleep -s 15
+    Start-Sleep -s 60
 
     Write-Output "INFO: Downloading chef bundle from s3 location: $SourceBucket/$ChefPath/$CookbookVersion/chef-bundle.tar.gz"
     Read-S3Object -Region $BucketRegion -BucketName $SourceBucket -Key /$ChefPath/$CookbookVersion/chef-bundle.tar.gz -File $GzipPath
